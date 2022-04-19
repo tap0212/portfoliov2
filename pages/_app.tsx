c97ab54cf8d000eee482ad13b118506b847f870c
tap0212 <tapan.awasthi02@gmail.com>
2022-04-19 13:49:35 +0530

    added ogs 🚀

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,30 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@500&display=swap"
           rel="stylesheet"></link>
+        <title>Tapan Awasthi</title>
+        <meta
+          name="description"
+          content="A software engineer living in who works with js and related
+                  technologies like React, React Native typescript and many
+                  more... A Potter head and a marvel fan"></meta>
+        <meta name="title" content="Tapan Awasthi"></meta>
+        <meta
+          property="og:image"
+          content="https://tapanawasthi.dev/images/me.png"
+        />
+
+        <meta property="og:title" content="Tapan Awasthi" />
+
+        <meta
+          property="og:description"
+          content="A software engineer living in who works with js and related
+                  technologies like React, React Native typescript and many
+                  more... A Potter head and a marvel fan"
+        />
+
+        <meta property="og:image:width" content="1080" />
+
+        <meta property="og:image:height" content="1080" />
       </Head>
       <Component {...pageProps} />
     </>
